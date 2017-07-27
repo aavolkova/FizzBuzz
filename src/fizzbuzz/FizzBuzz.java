5ad7ee018b585c96e4bec250415662a04b18b677
aavolkova <avolkova@montgomerycollege.edu>
2017-07-27 17:58:40 -0400

    Secomd commit

--- src/fizzbuzz/FizzBuzz.java
+++ src/fizzbuzz/FizzBuzz.java
@@ -12,7 +12,7 @@ public class FizzBuzz {
 		 * @author Anna Volkova
 		 */
 		
-		
+		/*
 		for (int counter = 1; counter <= 100; counter++)
 		{
 			if(counter % 3 == 0 && counter % 5 == 0)
@@ -24,8 +24,53 @@ public class FizzBuzz {
 			else
 				System.out.println(counter);     // Print number which is not multiples of three, or five, or both three and five
 		}
+		*/
 		
-
+		// Modification 1:  add: 2 - Copy, 10 - CAT
+		/*
+		for (int counter = 1; counter <= 100; counter++)
+		{
+			if(counter % 2 == 0 )
+				System.out.print("Copy");  // Print number which is multiples of two 
+			
+			if(counter % 10 == 0 )
+				System.out.print("Cat");  // Print number which is multiples of ten
+			
+			if (counter % 3 == 0)
+				System.out.print("Fizz");      // Print number which is multiples of three
+			
+			if (counter % 5 == 0)
+				System.out.print("Buzz");      // Print number which is multiples of five
+			
+			if(counter % 2 != 0 && counter % 10 != 0 && counter % 3 != 0 && counter % 5 != 0)
+				System.out.print(counter);     // Print number which is not multiples of two, three, five, or ten)
+			
+			System.out.println();
+			
+		}
+		*/
+		
+		
+		// Modification 2:  use Integer.toString(counter) instead System.out.print(counter)
+		for (int counter = 1; counter <= 100; counter++)
+		{
+			if(counter % 2 == 0 )
+				System.out.print("Copy");  		// Print Copy if number multiples of two 
+			
+			if(counter % 10 == 0 )
+				System.out.print("Cat");  		// Print Cat if number multiples of ten
+			
+			if (counter % 3 == 0)
+				System.out.print("Fizz");       // Print Fizz if number multiples of three
+			
+			if (counter % 5 == 0)
+				System.out.print("Buzz");       // Print Buzz if number multiples of five
+			
+			if(counter % 2 != 0 && counter % 10 != 0 && counter % 3 != 0 && counter % 5 != 0)
+				System.out.print(Integer.toString(counter));     // Print number which is not multiples of two, three, five, or ten)
+			
+			System.out.println();
+		}
 	}
 
 }
